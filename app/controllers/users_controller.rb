25956813e315117cfea0a5226fa31d37ed877f38
Caleb Siu <calebs19997@gmail.com>
2017-01-13 02:44:19 +0000

    finish User update functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
   
     def show
     @user = User.find(params[:id])
@@ -14,7 +17,6 @@ class UsersController < ApplicationController
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-      # Handle a successful save.
     else
       render 'new'
     end
@@ -24,4 +26,37 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+    
+    def edit
+      @user = User.find(params[:id])
+    end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  def logged_in_user
+    unless logged_in?
+    store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+  def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
+  end
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
 end
